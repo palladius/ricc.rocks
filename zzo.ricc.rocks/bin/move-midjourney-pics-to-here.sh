4ba7895eb23081299c391f9200207aed8ddb0132
Riccardo Carlesso <palladiusbonton@gmail.com>
2023-04-10 13:51:46 +0200

    piano-color

--- zzo.ricc.rocks/bin/move-midjourney-pics-to-here.sh
+++ zzo.ricc.rocks/bin/move-midjourney-pics-to-here.sh
@@ -6,3 +6,5 @@ echodo mv ~/Desktop/PalladiusFigata_*png static/gallery/midjourney/
 
 echodo mv ~/Downloads/PalladiusPacans_*png static/gallery/midjourney/
 echodo mv ~/Desktop/PalladiusPacans_*png static/gallery/midjourney/
+
+cd static/gallery/midjourney/ && doppioni .
